0458bda5c569fa4324c8d45b4e22f88ba371ec2d
ujwalkumar-mahajan <100671171+ujwalkumar-mahajan@users.noreply.github.com>
2023-10-27 15:14:26 +0530

    checking last commit

--- controller/product.js
+++ controller/product.js
@@ -24,27 +24,27 @@ exports.createProduct = (req, res) => {
 };
 // GET
 exports.getAllProducts = async (req, res) => {
-//  const product = await Product.find();
- let product = [
-  {
-    "id": 2,
-    "title": "iPhone X",
-    "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
-    "price": 899,
-    "discountPercentage": 17.94,
-    "rating": 4.44,
-    "stock": 34,
-    "brand": "Apple",
-    "category": "smartphones",
-    "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-    "images": [
-        "https://i.dummyjson.com/data/products/2/1.jpg",
-        "https://i.dummyjson.com/data/products/2/2.jpg",
-        "https://i.dummyjson.com/data/products/2/3.jpg",
-        "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
-    ]
-}
- ]
+ const product = await Product.find();
+//  let product = [
+//   {
+//     "id": 2,
+//     "title": "iPhone X",
+//     "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+//     "price": 899,
+//     "discountPercentage": 17.94,
+//     "rating": 4.44,
+//     "stock": 34,
+//     "brand": "Apple",
+//     "category": "smartphones",
+//     "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+//     "images": [
+//         "https://i.dummyjson.com/data/products/2/1.jpg",
+//         "https://i.dummyjson.com/data/products/2/2.jpg",
+//         "https://i.dummyjson.com/data/products/2/3.jpg",
+//         "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+//     ]
+// }
+//  ]
   res.status(200).json(product);
 };
 
